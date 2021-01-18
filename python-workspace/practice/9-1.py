98ea273eba9765060f2d047fd7fb899e921a9660
Siyun Lee <siyun1221@gmail.com>
2021-01-18 21:35:15 +0900

    Learn to chapter 9-7

--- python-workspace/practice/9-1.py
+++ python-workspace/practice/9-1.py
@@ -1,4 +1,5 @@
 #9-1 클래스
+'''
 #마린 : 공격 유닛, 군인. 총을 쏠 수 있음
 name = '마린' #유닛의 이름
 hp = 40 #유닛의 체력
@@ -16,7 +17,7 @@ print(f'{tank_name} 유닛이 생성되었습니다.')
 print(f'체력 {tank_hp}, 공격력 {tank_damage} \n')
 
 def attack(name, location, damage):
-    print(f'{name} : {location} 방향으로 적군을 공격합니다. 공격력 {damage}')
+    print(f'{name} : {location} 방향으로 적군을 공격합니다. [공격력 {damage}]')
 
 tank2_name = '탱크'
 tank2_hp = 150
@@ -28,3 +29,32 @@ print(f'체력 {tank2_hp}, 공격력 {tank2_damage} \n')
 attack(name, '1시', damage)
 attack(tank_name, '1시', tank_damage)
 attack(tank2_name, '1시', tank_damage)
+'''
+
+class Unit:
+    def __init__(self, name, hp, damage) : 
+        self.name = name
+        self.hp = hp
+        self.damage = damage
+        print(f'{self.name} 유닛이 생성 되었습니다.')
+        print(f'체력 {self.hp}, 공격력 {self.damage}')
+
+'''
+marine1 = Unit('마린', 40, 5)
+marine2 = Unit('마린', 40, 5)
+tank = Unit('탱크', 150, 35)
+
+# marine3 = Unit('마린')
+# marine3 = Unit('마린', 40)
+'''
+
+#레이스 : 공중 유닛, 비행기. 클로킹 (상대방에게 보이지 않음)
+wraith1 = Unit('레이스', 80, 5)
+print(f'유닛 이름 : {wraith1.name}, 공격력 : {wraith1.damage}')
+
+#마인드 컨트롤 : 상대방 유닛을 내것으로 만드는 것 (빼앗음)
+wraith2 = Unit('빼앗은 레이스', 80, 5)
+wraith2.clocking = True
+
+if wraith2.clocking == True:
+    print(f'{wraith2.name}는 현재 클로킹 상태입니다.')
